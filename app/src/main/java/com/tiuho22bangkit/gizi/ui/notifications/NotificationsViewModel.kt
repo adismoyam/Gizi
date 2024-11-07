bcef6b69b2e33f88ce0b098428436818ff806a05
adismoyam <adismoyam@gmail.com>
2024-11-07 17:48:24 +0800

    AAI

---
+++ app/src/main/java/com/tiuho22bangkit/gizi/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.tiuho22bangkit.gizi.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
