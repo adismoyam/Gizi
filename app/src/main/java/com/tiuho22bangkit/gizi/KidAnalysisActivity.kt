1b944e7d98b3620147d4a16e50034506165de42f
mrleos <rmuhreksa@gmail.com>
2024-11-24 23:03:11 +0800

    solve some bug/error, status:on progress

--- app/src/main/java/com/tiuho22bangkit/gizi/KidAnalysisActivity.kt
+++ app/src/main/java/com/tiuho22bangkit/gizi/KidAnalysisActivity.kt
@@ -1,5 +1,6 @@
 package com.tiuho22bangkit.gizi
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.tiuho22bangkit.gizi.data.local.KidEntity
 import com.tiuho22bangkit.gizi.databinding.ActivityKidAnalysisBinding
@@ -22,7 +24,6 @@ class KidAnalysisActivity : AppCompatActivity(), View.OnClickListener {
 
         binding = ActivityKidAnalysisBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setContentView(R.layout.activity_kid_analysis)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -59,8 +60,15 @@ class KidAnalysisActivity : AppCompatActivity(), View.OnClickListener {
             }
 
         }
-
-
+        binding.btnUbahData.setOnClickListener {
+            val intent = Intent(this, UpdateKidActivity::class.java)
+            intent.putExtra("name", kid?.name)
+            intent.putExtra("gender", kid?.gender)
+            intent.putExtra("birthdate", kid?.birthDate)
+            intent.putExtra("height", kid?.height ?: 0f)
+            intent.putExtra("weight", kid?.weight ?: 0f)
+            startActivity(intent)
+            }
     }
 
     override fun onClick(v: View?) {
